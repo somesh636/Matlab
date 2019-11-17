c8c3404376f8913ed4caed3885da66e29bf423f0
Somesh <somesh110296@gmail.com>
2019-11-17 16:48:43 -0500

    added folder

---
+++ Face-Recognition-Project-Matlab-Code/README.md
@@ -0,0 +1,6 @@
+# Face Recognition Project Matlab-Code
+ Face Recognition Project Matlab Code
+ 
+ Run :GuiMain.p
+ 
+ Need a Help ! just Email Us : hatem_hajri@matlab-recognition-code.com
---
+++ Face-Recognition-Project-Matlab-Code/sourcecode.m
@@ -0,0 +1,13 @@
+% 
+%Face Recognition based on Wavelet and Neural Networks
+% 
+% Feel Free To visit us :visi
+% http://matlab-recognition-code.com/face-recognition-based-on-wavelet-and-neural-networks-matlab-code/
+%
+% 
+% Face recognition based on Wavelet
+%                                 and Neural Networks
+%                               - High recognition rate
+%                               - Easy and intuitive GUI
+%
+% For more informations please email me ( hatem_hajri@matlab-recognition-code.com )
